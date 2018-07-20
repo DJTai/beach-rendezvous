06c26ac930d4821ec803dbab6e6df6f517823185
David T <djtai@protonmail.com>
2018-07-19 21:59:27 -0700

    Implement Fragments
    Create fragments and implement their creation via bottom nav bar selection. These fragments should suffice for the "dummy app"

--- app/src/main/java/com/example/beachrendezvous/MainActivity.java
+++ app/src/main/java/com/example/beachrendezvous/MainActivity.java
@@ -1,51 +1,76 @@
 package com.example.beachrendezvous;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import com.example.beachrendezvous.ui.InfoFragment;
+import com.example.beachrendezvous.ui.ProfileFragment;
+import com.example.beachrendezvous.ui.SettingsFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 // Butter Knife imports
 // see https://jakewharton.github.io/butterknife/ for more details
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "main_activity";
     public static final String DEBUG = "debug";
 
-    // Reference to TextView on UI
-    @BindView(R.id.message)
-    TextView mTextMessage;
-
     // Reference to the bottom nav bar
     @BindView(R.id.navigation)
     BottomNavigationView navigation;
 
+    /**
+     * Determines which fragment to open upon nav selection
+     */
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected (@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    // TODO: Open fragment
-                    mTextMessage.setText(R.string.nav_info);
-                    return true;
+            Fragment fragment;
+            int choice = item.getItemId();
 
-                case R.id.navigation_dashboard:
-                    // TODO: Open fragment
-                    mTextMessage.setText(R.string.nav_profile);
-                    return true;
+            if (choice == R.id.nav_info) {
+                fragment = new InfoFragment();
 
-                case R.id.navigation_notifications:
-                    // TODO: Open fragment
-                    mTextMessage.setText(R.string.nav_settings);
-                    return true;
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction
+                        .replace(R.id.frame_fragment, fragment)
+                        .commit();
+                return true;
+
+            } else if (choice == R.id.nav_profile) {
+                fragment = new ProfileFragment();
+
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction
+                        .replace(R.id.frame_fragment, fragment)
+                        .commit();
+
+                return true;
+
+            } else if (choice == R.id.nav_settings) {
+                fragment = new SettingsFragment();
+
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction
+                        .replace(R.id.frame_fragment, fragment)
+                        .commit();
+
+                return true;
             }
             return false;
         }
