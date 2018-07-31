4ce884d1c934659a0f5223ba74f42ef18138f7a4
David T <djtai@protonmail.com>
2018-07-31 11:39:05 -0700

    Delete unused code

--- app/src/main/java/com/example/beachrendezvous/fragments/SubMenu.java
+++ app/src/main/java/com/example/beachrendezvous/fragments/SubMenu.java
@@ -32,11 +32,7 @@ public class SubMenu extends Fragment {
     private static final String TAG = "sub_menu_fragment";
     private static final String ARG_PARAM = "param";
     private static final String ARG_PARAM1 = "param1";
-
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM2 = "param2";
-//
+    
     /* Parameter to determine the context of the SubMenu fragment, e.g., Creating or Searching */
     private String mParam;
 
