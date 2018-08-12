5dc313d58931014241c7b9a8374d6a6d8b48ed11
Chaitanya Vooradi <vooradichaitanya@gmail.com>
2018-08-12 14:34:35 -0700

    both created and joined events displayed on home screen

--- app/src/main/java/com/example/beachrendezvous/fragments/SportsSearchDetails.java
+++ app/src/main/java/com/example/beachrendezvous/fragments/SportsSearchDetails.java
@@ -9,8 +9,11 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.beachrendezvous.MainActivity;
 import com.example.beachrendezvous.R;
 import com.example.beachrendezvous.database.SportsEntity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -29,10 +32,12 @@ public class SportsSearchDetails extends Fragment {
 //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";  // param1 gives the type of sport
     private static final String ARG_PARAM2 = "param2";
-
+    String event_id;
+    String name;
     Unbinder mUnbinder;
     View view = null;
     SportsEntity sportsEntity;
+    private static final String EVENT_ID="event_id";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -41,6 +46,12 @@ public class SportsSearchDetails extends Fragment {
     @OnClick(R.id.sportsSearch_btn)
     void joinClicked() {
         Toast.makeText(getContext(), "join clicked", Toast.LENGTH_SHORT).show();
+        Log.d("name",name);
+        Log.d("event id",event_id);
+        Log.d("mparam1",mParam1);
+        DatabaseReference mDatabaseReference= FirebaseDatabase.getInstance().getReference().child("Users").child(name).child("Event_Id");
+        mDatabaseReference.child(event_id).setValue(mParam1);
+
 
 
     }
@@ -54,6 +65,10 @@ public class SportsSearchDetails extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             //mParam2 = getArguments().getString(ARG_PARAM2);
+            event_id=getArguments().getString(EVENT_ID);
+            name=getArguments().getString(MainActivity.ARG_GIVEN_NAME);
+
+
             sportsEntity = (SportsEntity) getArguments().getSerializable("entityObject");
         }
     }
