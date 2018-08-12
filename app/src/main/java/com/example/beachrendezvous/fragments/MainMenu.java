c7b8492fc6ed996b59e685a04018eb3b9105c202
Chaitanya Vooradi <vooradichaitanya@gmail.com>
2018-08-11 21:13:14 -0700

    home layout and event id creation in users table

--- app/src/main/java/com/example/beachrendezvous/fragments/MainMenu.java
+++ app/src/main/java/com/example/beachrendezvous/fragments/MainMenu.java
@@ -6,9 +6,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.beachrendezvous.MainActivity;
 import com.example.beachrendezvous.R;
+import com.example.beachrendezvous.database.SportsEntity;
+import com.example.beachrendezvous.sportsEventDetailsAdapter;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -30,6 +40,15 @@ public class MainMenu extends Fragment {
     private String mParam2;
 
     private Unbinder mUnbinder;
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mDatabaseReference;
+    ChildEventListener eventchildEventListener;
+    String name;
+    ListView mListView;
+    ArrayList<String> type1 = new ArrayList<>();
+    ArrayList<String> date1 = new ArrayList<>();
+    ArrayList<String> place1 = new ArrayList<>();
+
 
     public MainMenu() {
         // Required empty public constructor
@@ -38,12 +57,14 @@ public class MainMenu extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
 
-        // TODO: Get reference to DB
+        if (getArguments() != null) {
+                     name = getArguments().getString(MainActivity.ARG_GIVEN_NAME);
+
+        }
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(name).child("Event_Id");
+        Log.d("Menu datbasereference",mDatabaseReference.getKey());
+        attachDatabaseReadListener();
 
     }
 
@@ -55,10 +76,11 @@ public class MainMenu extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_main_menu, container, false);
 
-        // TODO: Show events
+        mListView = (ListView) view.findViewById(R.id.mainMenu_listview);
 
-        // Bind View using ButterKnife
+
         mUnbinder = ButterKnife.bind(this, view);
+        attachDatabaseReadListener();
 
         return view;
     }
@@ -70,28 +92,64 @@ public class MainMenu extends Fragment {
         // Unbind the view to free some memory
         mUnbinder.unbind();
     }
+    private void attachDatabaseReadListener() {
+        // Only attach DB if there is no ChildEventListener
+        Log.d("in mchildeventlistner", " test");
+        if (eventchildEventListener == null) {
+            eventchildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    // Called whenever a new msg is inserted in the msg's list
+                    // Triggered for every child msg in the list when the listener is first attached
+                    // - DataSnapshot will contain the message that just got added to the DB
 
-    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed (Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-//
-//    @Override
-//    public void onAttach (Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                                               + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach () {
-//        super.onDetach();
-//        mListener = null;
-//    }
+                   for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                        //String friend = ds.getKey();
+                       // DatabaseReference mDatabaseReference1=FirebaseDatabase.getInstance().getReference().child("Users").child(name).child("Event_Id");
+                      //  mDatabaseReference.child(id).setValue(true);
+                       Log.d("In main menu ", ds.getKey());
+                    }
+/*
+                    SportsEntity sportsevent = dataSnapshot.getValue(
+                            SportsEntity.class);    // Deserialize msg from DB -> POJO
+
+                    type1.add(sportsevent.getType());
+                    date1.add(sportsevent.getDate());
+                    place1.add(sportsevent.getLocation());
+                    sportsEventDetailsAdapter myAdapter = new sportsEventDetailsAdapter(
+                            getContext().getApplicationContext(), type1, date1, place1,
+                            sportsevent.getCreated_by());
+                    //sportsEventDetailsAdapter m=new sportsEventDetailsAdapter(getContext()
+                    // .getApplicationContext(), )
+                   // entityObjects.add(sportsevent);
+                    mListView.setAdapter(myAdapter);
+                    */
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    // Called when the contents of an existing msg  is changed
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    // Called when a msg is deleted
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                    // Called when a msg changes position in the list
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // Called when an error occurred while trying to make changes
+                    // Usually called when permission is denied to read data
+                }
+            };
+            mDatabaseReference.addChildEventListener(eventchildEventListener);
+        }
+    }
+
+
 }
