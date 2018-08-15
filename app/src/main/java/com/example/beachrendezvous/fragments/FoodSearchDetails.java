5a7dc5a88d1faba63c7f002b1aee4487f2a76fa4
David T <djtai@protonmail.com>
2018-08-15 12:17:15 -0700

    Food Search UI Fixes

--- app/src/main/java/com/example/beachrendezvous/fragments/FoodSearchDetails.java
+++ app/src/main/java/com/example/beachrendezvous/fragments/FoodSearchDetails.java
@@ -1,47 +1,67 @@
 package com.example.beachrendezvous.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.beachrendezvous.MainActivity;
 import com.example.beachrendezvous.R;
+import com.example.beachrendezvous.database.FoodEntity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class FoodSearchDetails extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+
+    //region Bundle paramters
+    private static final String ARG_PARAM1 = "param1";  // Refers to restaurant
+    private static final String ARG_PARAM2 = "param2";  // Refers to ???
+    private static final String EVENT_ID = "event_id";
+    private static final String ENTITY_OBJECT = "entityObject";
     private String mParam1;
     private String mParam2;
+    //endregion
 
     //region References
     private Unbinder mUnbinder;
+    private String event_id;
+    private String name;
+    private FoodEntity foodEntity;
+    //endregion
+
+    //region ButterKnife binds
+    @BindView(R.id.foodSearch_eventType)
+    TextView eventType;
+
+    @BindView(R.id.foodSearch_dateText)
+    TextView dateText;
+
+    @BindView(R.id.foodSearch_placeText)
+    TextView placeText;
+
+    @BindView(R.id.foodSearch_timeText)
+    TextView timeText;
+
+    @BindView(R.id.foodSearch_numText)
+    TextView numText;
+
+    @BindView(R.id.foodSearch_commentText)
+    TextView commentText;
+
+    @BindView(R.id.foodSearch_durationText)
+    TextView durationText;
+
+    @BindView(R.id.foodSearch_limitText)
+    TextView limitText;
     //endregion
 
     public FoodSearchDetails() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FoodSearchDetails.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FoodSearchDetails newInstance(String param1, String param2) {
-        FoodSearchDetails fragment = new FoodSearchDetails();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
@@ -49,15 +69,27 @@ public class FoodSearchDetails extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+            event_id = getArguments().getString(EVENT_ID);
+            name = getArguments().getString(MainActivity.ARG_GIVEN_NAME);
+
+            foodEntity = (FoodEntity) getArguments().getSerializable(ENTITY_OBJECT);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_search_details, container, false);
+        View view;
+
+        view = inflater.inflate(R.layout.fragment_food_search_details, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+
+        if (mParam1 != null) {
+
+        }
+
+        return view;
     }
 
     @Override
